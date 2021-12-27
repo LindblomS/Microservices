fa49b5be748301f242e22dcef9839f4df3ea6095
Samuel Lindblom <samuel.lindblom@hotmail.com>
2021-12-27 21:30:55 +0100

    wip

---
+++ src/Payment/Payment.API/IntegrationEventHandlers/OrderStatusChangedToStockConfirmedIntegrationEventHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Payment.API.IntegrationEventHandlers;
+
+using EventBus.EventBus.Abstractions;
+using Ordering.Contracts.IntegrationEvents;
+using System.Threading.Tasks;
+
+public class OrderStatusChangedToStockConfirmedIntegrationEventHandler : IIntegrationEventHandler<OrderStatusChangedToStockConfirmedIntegrationEvent>
+{
+    readonly IEventBus eventBus;
+    readonly ILogger<OrderStatusChangedToStockConfirmedIntegrationEventHandler> logger;
+
+    public OrderStatusChangedToStockConfirmedIntegrationEventHandler(
+        IEventBus eventBus,
+        ILogger<OrderStatusChangedToStockConfirmedIntegrationEventHandler> logger)
+    {
+        this.eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    public Task Handle(OrderStatusChangedToStockConfirmedIntegrationEvent @event)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Payment/Payment.API/Startup.cs
@@ -0,0 +1,63 @@
+﻿namespace Payment.API;
+
+using Autofac;
+using EventBus.EventBus;
+using EventBus.EventBus.Abstractions;
+using EventBus.EventBusRabbitMQ;
+using RabbitMQ.Client;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEventBus(Configuration);
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+
+    }
+}
+
+internal static class Extensions
+{
+    public static IServiceCollection AddEventBus(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+        {
+            var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = configuration["EventBus:Connection"],
+                DispatchConsumersAsync = true
+            };
+
+            return new DefaultRabbitMQPersistentConnection(factory, logger, 5);
+        });
+
+        services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
+        {
+            var subscriptionClientName = configuration["EventBus:SubscriptionClientName"];
+            var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+            var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+            var logger = sp.GetRequiredService<ILogger<EventBusRabbitMQ>>();
+            var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+
+            return new EventBusRabbitMQ(rabbitMQPersistentConnection, logger, iLifetimeScope, eventBusSubcriptionsManager, subscriptionClientName, 5);
+        });
+
+        services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
+
+        return services;
+    }
+}
+
+
